2c5556cd977b1d071da9fa32c12df3d6e2658e1c
b0518n <b0518n@naver.com>
2023-06-08 11:19:37 +0900

    동적 계획법 1

--- BackJoon/10844.cs
+++ BackJoon/10844.cs
@@ -1,71 +1,36 @@
 ﻿int n = int.Parse(Console.ReadLine());
-int result = 0;
-long[] arr = new long[101];
-Dictionary<int, int> dic = new Dictionary<int, int>();
-Dictionary<int, int> tempDic = new Dictionary<int, int>();
+int[] arr = new int[n + 1];
+int[] result = new int[n + 1];
+int input = 0;
+int max = 0;
 
-arr[1] = 9;
-for (int i = 0; i <= 9; i++)
+for (int i = 1; i <= n; i++)
 {
-    if (i == 0)
+    input = int.Parse(Console.ReadLine());
+    arr[i] = input;
+
+    if (i == 1)
     {
-        dic.Add(i, 0);
+        result[i] = arr[i];
+        max = result[i];
+    }
+    else if (i == 2)
+    {
+        result[i] = arr[i - 1] + arr[i];
+    }
+    else if (i == 3)
+    {
+        result[i] = Math.Max(arr[i - 2] + arr[i], arr[i - 1] + arr[i]);
     }
     else
     {
-        dic.Add(i, 1);
+        result[i] = Math.Max(Math.Max(result[i - 2] + arr[i], result[i - 3] + arr[i - 1] + arr[i]), result[i - 1]);
     }
 
-    tempDic.Add(i, 0);
-}
-
-for (int i = 2; i < 101; i++)
-{
-    foreach (int temp in dic.Keys)
+    if (max < result[i])
     {
-        int key1 = temp + 1;
-        int key2 = temp - 1;
-        int value = dic[temp] % 1000000000;
-
-        if (temp == 0)
-        {
-            arr[i] += value;
-            tempDic[key1] += value;
-        }
-        else if (temp == 9)
-        {
-            arr[i] += value;
-            tempDic[key2] += value;
-        }
-        else
-        {
-            arr[i] += (value * 2);
-            tempDic[key1] += value;
-            tempDic[key2] += value;
-        }
-    }
-
-    dic = Copy(tempDic);
-    Clear(tempDic);
-}
-
-Console.WriteLine(arr[n] % 1000000000);
-
-Dictionary<int, int> Copy(Dictionary<int, int> dic)
-{
-    Dictionary<int, int> temp = new Dictionary<int, int>();
-    foreach (int tmp in dic.Keys)
-    {
-        temp.Add(tmp, dic[tmp]);
-    }
-
-    return temp;
-}
-
-void Clear(Dictionary<int, int> dic)
-{
-    foreach (int temp in dic.Keys)
-    {
-        dic[temp] = 0;
+        max = result[i];
     }
 }
+
+Console.WriteLine(max);
---
+++ BackJoon/2156.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+List<int> list1 = new List<int>();
+List<int> list2 = new List<int>();
+list1.Add(0);
+list2.Add(0);
+
+for (int i = 0; i < n; i++)
+{
+    if (i == 0)
+    {
+        list1.Add(arr[i]);
+        list2.Add(i + 1);
+    }
+    else
+    {
+        for (int j = list1.Count - 1; j >= 0; j--)
+        {
+            if (list1[j] < arr[i])
+            {
+                list2.Add(j + 1);
+                if (j == list1.Count - 1)
+                {
+                    list1.Add(arr[i]);
+                }
+
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(list2.Max());
