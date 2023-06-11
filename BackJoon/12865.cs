1e7aa287d53a5dd702610c450545f54037a87804
b0518n <b0518n@naver.com>
2023-06-11 11:31:10 +0900

    동적 계획법 1

---
+++ BackJoon/12865.cs
@@ -0,0 +1,49 @@
+﻿string str1 = Console.ReadLine();
+string str2 = Console.ReadLine();
+
+int count = 0;
+int max = 0;
+int k = 0;
+int previousIndex = 0;
+for (int i = 0; i < str1.Length; i++)
+{
+    count = 0;
+    k = 0;
+    previousIndex = 0;
+
+    for (int j = i; j < str1.Length; j++)
+    {
+        while (true)
+        {
+            if (str1[j] == str2[k])
+            {
+                count++;
+                previousIndex = k;
+                k = previousIndex + 1;
+                break;
+            }
+
+            if (k == str2.Length - 1)
+            {
+                k = previousIndex + 1;
+                break;
+            }
+            else
+            {
+                k++;
+            }
+        }
+
+        if (previousIndex == str2.Length - 1)
+        {
+            break;
+        }
+    }
+
+    if (max < count)
+    {
+        max = count;
+    }
+}
+
+Console.WriteLine(max);
---
+++ BackJoon/9251.cs
@@ -0,0 +1,24 @@
+﻿string str1 = Console.ReadLine();
+string str2 = Console.ReadLine();
+
+int m = str1.Length;
+int n = str2.Length;
+
+int[,] arr = new int[n + 1, m + 1];
+
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = 1; j < m + 1; j++)
+    {
+        if (str1[j - 1] == str2[i - 1])
+        {
+            arr[i, j] = arr[i - 1, j - 1] + 1;
+        }
+        else
+        {
+            arr[i, j] = Math.Max(arr[i - 1, j], arr[i, j - 1]);
+        }
+    }
+}
+
+Console.WriteLine(arr[n, m]);
