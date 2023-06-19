9141fd6ff6b2645120b56bd00a2d4691cbc012b3
b0518n <b0518n@naver.com>
2023-06-19 14:56:13 +0900

    분할 정복

---
+++ BackJoon/2740.cs
@@ -0,0 +1,58 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+
+int[,] a = new int[n, m];
+for (int i = 0; i < n; i++)
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < input.Length; j++)
+    {
+        a[i, j] = input[j];
+    }
+}
+
+input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+m = input[0];
+int k = input[1];
+
+int[,] b = new int[m, k];
+for (int i = 0; i < m; i++)
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < input.Length; j++)
+    {
+        b[i, j] = input[j];
+    }
+}
+
+int[,] arr = new int[n, k];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < k; j++)
+    {
+        for (int l = 0; l < m; l++)
+        {
+            arr[i, j] += a[i, l] * b[l, j];
+            if (l == m - 1)
+            {
+                if (j == k - 1)
+                {
+                    sw.WriteLine(arr[i, j]);
+                }
+                else
+                {
+                    sw.Write(arr[i, j] + " ");
+                }
+            }
+        }
+    }
+}
+
+sw.Flush();
+sw.Close();
+sr.Close();
