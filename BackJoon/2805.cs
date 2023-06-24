657f694219b32a86cb296ef7a23e72debefea8d7
b0518n <b0518n@naver.com>
2023-06-24 16:46:07 +0900

    이분 탐색

---
+++ BackJoon/2805.cs
@@ -0,0 +1,41 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+
+long[] trees = Array.ConvertAll(sr.ReadLine().Split(), long.Parse);
+
+long start = 0;
+long end = trees.Max();
+long max = 0;
+
+while (start <= end)
+{
+    long mid = (start + end) / 2;
+    long value = 0;
+    for (int i = 0; i < trees.Length; i++)
+    {
+        if (trees[i] > mid)
+        {
+            value += trees[i] - mid;
+        }
+    }
+
+    if (value >= m)
+    {
+        start = mid + 1;
+        max = Math.Max(max, mid);
+    }
+    else
+    {
+        end = mid - 1;
+    }
+}
+
+
+sw.WriteLine(max);
+sw.Flush();
+sw.Close();
+sr.Close();
