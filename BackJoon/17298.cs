f19ef6b82286a4e59d9e4dc767fc9f5cfdfad422
b0518n <b0518n@naver.com>
2023-07-08 03:08:03 +0900

    스택2

---
+++ BackJoon/17298.cs
@@ -0,0 +1,36 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int n = int.Parse(Console.ReadLine());
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] nges = new int[n];
+List<int> stack = new List<int>();
+
+for (int i = 0; i < nges.Length; i++)
+{
+    nges[i] = -1;
+}
+
+for (int i = 0; i < input.Length; i++)
+{
+    while (stack.Count != 0 && input[stack[stack.Count - 1]] < input[i])
+    {
+        nges[stack[stack.Count - 1]] = input[i];
+        stack.RemoveAt(stack.Count - 1);
+    }
+
+    stack.Add(i); // 값이 아닌 인덱스를 넣음.
+}
+
+for (int i = 0; i < nges.Length; i++)
+{
+    if (i == 0)
+    {
+        sw.Write(nges[i]);
+    }
+    else
+    {
+        sw.Write(" " + nges[i]);
+    }
+}
+
+sw.Flush();
+sw.Close();
