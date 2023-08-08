18791ebc4b5443bcd283afaa3ae699ad97372ad1
b0518n <b0518n@naver.com>
2023-08-08 09:44:04 +0900

    투 포인터

---
+++ BackJoon/3273.cs
@@ -0,0 +1,31 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int n = int.Parse(Console.ReadLine());
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Dictionary<int, int> dics = new Dictionary<int, int>();
+for (int i = 0; i < arr.Length; i++)
+{
+    dics.Add(arr[i], 1);
+}
+
+int x = int.Parse(Console.ReadLine());
+int count = 0;
+int value = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    value = x - arr[i];
+
+    if (arr[i] == value)
+    {
+        continue;
+    }
+
+    if (dics.ContainsKey(value))
+    {
+        count++;
+    }
+}
+
+sw.WriteLine(count / 2);
+sw.Flush();
+sw.Close();
