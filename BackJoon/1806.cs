07ed2157ea630c66360416abd66086ad6e43f01f
b0518n <b0518n@naver.com>
2023-08-10 07:34:50 +0900

    투 포인터

---
+++ BackJoon/1806.cs
@@ -0,0 +1,64 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int s = input[1];
+
+int[] sequence = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int start = 0;
+int end = 0;
+int value = sequence[start];
+int minLength = int.MaxValue;
+int length = 1;
+
+while (true)
+{
+    if (s > value)
+    {
+        if (end == n - 1)
+        {
+            break;
+        }
+
+        end++;
+        value += sequence[end];
+        length++;
+    }
+    else
+    {
+        if (minLength > length)
+        {
+            minLength = length;
+        }
+
+        if (end > start)
+        {
+            value -= sequence[start];
+            start++;
+            length--;
+        }
+        else
+        {
+            if (end == n - 1)
+            {
+                break;
+            }
+
+            end++;
+            value += sequence[end];
+            length++;
+
+        }
+    }
+}
+
+if (minLength == int.MaxValue)
+{
+    sw.WriteLine(0);
+}
+else
+{
+    sw.WriteLine(minLength);
+}
+
+sw.Flush();
+sw.Close();
