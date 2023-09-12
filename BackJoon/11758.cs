155e85e6026576d427a9bfb85451c6c606d0f6ec
b0518n <b0518n@naver.com>
2023-09-13 07:13:13 +0900

    기하2

---
+++ BackJoon/11758.cs
@@ -0,0 +1,43 @@
+﻿int[] input = null;
+
+List<int[]> vertexs = new List<int[]>();
+
+for (int i = 0; i < 3; i++)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    vertexs.Add(new int[2] { input[0], input[1] });
+}
+
+vertexs.Add(new int[2] { vertexs[0][0], vertexs[0][1] });
+
+int cost1 = 0;
+int cost2 = 0;
+
+int x = vertexs[0][0];
+int y = vertexs[0][1];
+
+for (int i = 1; i < vertexs.Count; i++)
+{
+    x *= vertexs[i][1];
+    y *= vertexs[i][0];
+
+    cost1 += x;
+    cost2 += y;
+
+    x = vertexs[i][0];
+    y = vertexs[i][1];
+}
+
+int value = cost2 - cost1;
+if (value < 0)
+{
+    Console.WriteLine(1);
+}
+else if (value == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(-1);
+}
