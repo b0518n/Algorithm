7c32f061f95732c17fd2ffeae039d1d946cbc340
b0518n <b0518n@naver.com>
2023-09-17 05:09:46 +0900

    구현

---
+++ BackJoon/2744.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+char[] chars = new char[input.Length];
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] >= 97)
+    {
+        chars[i] = (char)(input[i] - 97 + 65);
+    }
+    else
+    {
+        chars[i] = (char)(input[i] - 65 + 97);
+    }
+}
+
+Console.WriteLine(string.Join("", chars));
---
+++ BackJoon/2754.cs
@@ -0,0 +1,56 @@
+﻿string input = Console.ReadLine();
+float value = GetScore(input);
+string result = value.ToString("0.0");
+Console.WriteLine(result);
+
+float GetScore(string str)
+{
+    if (str.Length == 1)
+    {
+        return 0.0f;
+    }
+
+    float result = 0.0f;
+
+    switch (str)
+    {
+        case "A+":
+            result = 4.3f;
+            break;
+        case "A0":
+            result = 4.0f;
+            break;
+        case "A-":
+            result = 3.7f;
+            break;
+        case "B+":
+            result = 3.3f;
+            break;
+        case "B0":
+            result = 3.0f;
+            break;
+        case "B-":
+            result = 2.7f;
+            break;
+        case "C+":
+            result = 2.3f;
+            break;
+        case "C0":
+            result = 2.0f;
+            break;
+        case "C-":
+            result = 1.7f;
+            break;
+        case "D+":
+            result = 1.3f;
+            break;
+        case "D0":
+            result = 1.0f;
+            break;
+        case "D-":
+            result = 0.7f;
+            break;
+    }
+
+    return result;
+}
