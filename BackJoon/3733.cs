36ebd80c8dd207c87f142daf3daaef4ee3b3a36e
b0518n <b0518n@naver.com>
2023-09-18 06:00:39 +0900

    수학

---
+++ BackJoon/3733.cs
@@ -0,0 +1,17 @@
+﻿StringBuilder sb = new StringBuilder();
+string input = null;
+int[] arr = null;
+
+while (true)
+{
+    input = Console.ReadLine();
+    if (input == null || input == string.Empty)
+    {
+        break;
+    }
+
+    arr = Array.ConvertAll(input.Split(), int.Parse);
+    sb.AppendLine((arr[1] / (arr[0] + 1)).ToString());
+}
+
+Console.WriteLine(sb.ToString());
