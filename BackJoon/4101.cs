a9b167e6feeb4ac841fc98ed5fba5efe2b36fdfe
b0518n <b0518n@naver.com>
2023-09-18 06:00:05 +0900

    구현

---
+++ BackJoon/4101.cs
@@ -0,0 +1,25 @@
+﻿StringBuilder sb = new StringBuilder();
+int[] input = null;
+int a = 0;
+int b = 0;
+
+while (true)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    a = input[0];
+    b = input[1];
+
+    if (a == 0 && b == 0)
+    {
+        break;
+    }
+
+    sb.AppendLine(CompareValue(a, b));
+}
+
+Console.WriteLine(sb.ToString());
+
+string CompareValue(int a, int b)
+{
+    return a > b ? "Yes" : "No";
+}
---
+++ BackJoon/4999.cs
@@ -0,0 +1,38 @@
+﻿string str1 = Console.ReadLine();
+string str2 = Console.ReadLine();
+
+char[] arr1 = new char[1000];
+char[] arr2 = new char[1000];
+
+for (int i = 0; i < str1.Length - 1; i++)
+{
+    arr1[i] = str1[i];
+}
+
+for (int i = 0; i < str2.Length - 1; i++)
+{
+    arr2[i] = str2[i];
+}
+
+for (int i = 0; i < arr2.Length; i++)
+{
+    if (arr1[i] == arr2[i] && arr1[i] == 0)
+    {
+        Console.WriteLine("go");
+        break;
+    }
+    else if (arr1[i] == arr2[i] && arr1[i] == 'a')
+    {
+        continue;
+    }
+    else if (arr1[i] != arr2[i] && arr1[i] == 0)
+    {
+        Console.WriteLine("no");
+        break;
+    }
+    else if (arr1[i] != arr2[i] && arr2[i] == 0)
+    {
+        Console.WriteLine("go");
+        break;
+    }
+}
