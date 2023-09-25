38b536c3232d1e383352a04629b940de63446e75
b0518n <b0518n@naver.com>
2023-09-26 02:26:18 +0900

    Brute Force

---
+++ BackJoon/10819.cs
@@ -0,0 +1,42 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+bool[] visited = new bool[arr.Length];
+int[] order = new int[arr.Length];
+int max = 0;
+Recursion(0);
+Console.WriteLine(max);
+
+void Recursion(int k)
+{
+    if (k == n)
+    {
+        Calculate();
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i])
+        {
+            continue;
+        }
+
+        visited[i] = true;
+        order[k] = i;
+        Recursion(k + 1);
+        visited[i] = false;
+
+    }
+}
+
+void Calculate()
+{
+    int value = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        value += Math.Abs(arr[order[i]] - arr[order[i + 1]]);
+    }
+
+    max = Math.Max(max, value);
+}
