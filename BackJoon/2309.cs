b5c53e7612d28ada07d203e7b507528cd017ef82
b0518n <b0518n@naver.com>
2023-09-28 05:17:14 +0900

    Brute Force

---
+++ BackJoon/2309.cs
@@ -0,0 +1,30 @@
+﻿int sum = 0;
+List<int> list = new List<int>();
+int n = 0;
+
+for (int i = 0; i < 9; i++)
+{
+    n = int.Parse(Console.ReadLine());
+    list.Add(n);
+    sum += n;
+}
+
+sum -= 100;
+
+
+for (int i = 0; i < list.Count; i++)
+{
+    for (int j = i + 1; j < list.Count; j++)
+    {
+        if (list[i] + list[j] == sum)
+        {
+            list.Remove(list[i]);
+            list.Remove(list[j - 1]);
+            goto outerLoop;
+        }
+    }
+}
+
+outerLoop:
+list.Sort();
+Console.WriteLine(string.Join("\n", list));
