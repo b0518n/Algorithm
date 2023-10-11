68e9950aa695c37ac2171419023866e4d9c04030
b0518n <b0518n@naver.com>
2023-10-11 22:22:42 +0900

    DFS

---
+++ BackJoon/10026.cs
@@ -0,0 +1,141 @@
+﻿int n = int.Parse(Console.ReadLine());
+string input = null;
+char[,] dp = new char[n, n];
+char[,] _dp = new char[n, n];
+
+int[,] visited = new int[n, n];
+int[,] _visited = new int[n, n];
+
+int[] dy = new int[4] { 0, 0, -1, 1 };
+int[] dx = new int[4] { -1, 1, 0, 0 };
+
+int normalCase = 0;
+int uniqueCase = 0;
+
+for (int i = 0; i < n; i++)
+{
+    input = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        dp[i, j] = input[j];
+        _dp[i, j] = input[j];
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (visited[i, j] == 0)
+        {
+            DFS(i, j, dp[i, j]);
+        }
+
+        if (_visited[i, j] == 0)
+        {
+            _DFS(i, j, _dp[i, j]);
+        }
+    }
+}
+
+Console.WriteLine($"{normalCase} {uniqueCase}");
+
+// 적록색약일경우
+void DFS(int y, int x, char _char)
+{
+    Stack<int[]> stack = new Stack<int[]>();
+    stack.Push(new int[2] { y, x });
+    visited[y, x] = 1;
+
+    int[] temp = null;
+    int ny = 0;
+    int nx = 0;
+
+    while (stack.Count > 0)
+    {
+        temp = stack.Pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            ny = temp[0] + dy[i];
+            nx = temp[1] + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= n || nx >= n)
+            {
+                continue;
+            }
+
+            if (visited[ny, nx] == 1)
+            {
+                continue;
+            }
+
+            if (_char == 'R' || _char == 'G')
+            {
+                if (dp[ny, nx] == 'R' || dp[ny, nx] == 'G')
+                {
+                    visited[ny, nx] = 1;
+                    stack.Push(new int[2] { ny, nx });
+                }
+            }
+            else if (_char == 'B' && dp[ny, nx] == 'B')
+            {
+                visited[ny, nx] = 1;
+                stack.Push(new int[2] { ny, nx });
+            }
+        }
+    }
+
+    uniqueCase++;
+}
+
+// 정상인의 경우
+void _DFS(int y, int x, char _char)
+{
+    Stack<int[]> stack = new Stack<int[]>();
+    stack.Push(new int[2] { y, x });
+    _visited[y, x] = 1;
+
+    int[] temp = null;
+    int ny = 0;
+    int nx = 0;
+
+    while (stack.Count > 0)
+    {
+        temp = stack.Pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            ny = temp[0] + dy[i];
+            nx = temp[1] + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= n || nx >= n)
+            {
+                continue;
+            }
+
+            if (_visited[ny, nx] == 1)
+            {
+                continue;
+            }
+
+            if (_char == 'R' && dp[ny, nx] == 'R')
+            {
+                _visited[ny, nx] = 1;
+                stack.Push(new int[2] { ny, nx });
+            }
+            else if (_char == 'G' && dp[ny, nx] == 'G')
+            {
+                _visited[ny, nx] = 1;
+                stack.Push(new int[2] { ny, nx });
+            }
+            else if (_char == 'B' && dp[ny, nx] == 'B')
+            {
+                _visited[ny, nx] = 1;
+                stack.Push(new int[2] { ny, nx });
+            }
+        }
+    }
+
+    normalCase++;
+}
---
+++ BackJoon/4963.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int[] input = null;
+int w = 0;
+int h = 0;
+int[,] dp = null;
+int[,] visited = null;
+int result = 0;
+int[] dy = new int[8] { 0, 0, -1, -1, -1, 1, 1, 1 };
+int[] dx = new int[8] { -1, 1, 0, -1, 1, 0, -1, 1 };
+
+while (true)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    w = input[0];
+    h = input[1];
+
+    if (w == 0 && h == 0)
+    {
+        break;
+    }
+
+    dp = new int[h, w];
+    visited = new int[h, w];
+
+    for (int i = 0; i < h; i++)
+    {
+        input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        for (int j = 0; j < w; j++)
+        {
+            dp[i, j] = input[j];
+        }
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (visited[i, j] == 1 || dp[i, j] == 0)
+            {
+                continue;
+            }
+
+            DFS(i, j);
+        }
+    }
+
+    sb.AppendLine(result.ToString());
+    result = 0;
+}
+
+Console.WriteLine(sb.ToString());
+
+void DFS(int y, int x)
+{
+    Stack<int[]> stack = new Stack<int[]>();
+    stack.Push(new int[2] { y, x });
+    visited[y, x] = 1;
+    int[] temp = null;
+    int ny = 0;
+    int nx = 0;
+
+    while (stack.Count > 0)
+    {
+        temp = stack.Pop();
+        for (int i = 0; i < 8; i++)
+        {
+            ny = temp[0] + dy[i];
+            nx = temp[1] + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= h || nx >= w)
+            {
+                continue;
+            }
+
+            if (visited[ny, nx] == 1 || dp[ny, nx] == 0)
+            {
+                continue;
+            }
+
+            stack.Push(new int[2] { ny, nx });
+            visited[ny, nx] = 1;
+        }
+    }
+
+    result++;
+}
