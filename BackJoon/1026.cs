731c53327f56eae142c85fefa3ee121dd7ae37ea
b0518n <b0518n@naver.com>
2023-10-12 07:19:29 +0900

    Grid Algorithm

---
+++ BackJoon/1026.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] b = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+Array.Sort(a);
+Array.Reverse(a);
+Array.Sort(b);
+
+int result = 0;
+
+for (int i = 0; i < n; i++)
+{
+    result += a[i] * b[i];
+}
+
+Console.WriteLine(result);
---
+++ BackJoon/5585.cs
@@ -0,0 +1,16 @@
+﻿int cost = 1000 - int.Parse(Console.ReadLine());
+int[] coins = new int[6] { 500, 100, 50, 10, 5, 1 };
+int result = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    if (cost / coins[i] == 0)
+    {
+        continue;
+    }
+
+    result += cost / coins[i];
+    cost = cost % coins[i];
+}
+
+Console.WriteLine(result);
