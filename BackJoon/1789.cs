5be1e4485c1d1805ddc446975e7966f77239a7fb
b0518n <b0518n@naver.com>
2023-10-13 22:20:30 +0900

    Grid Algorithm

---
+++ BackJoon/1789.cs
@@ -0,0 +1,21 @@
+﻿long s = long.Parse(Console.ReadLine());
+long start = 1;
+long end = uint.MaxValue;
+long mid = 0;
+long result = 0;
+
+while (start <= end)
+{
+    mid = (start + end) / 2;
+    result = mid * (mid + 1) / 2;
+    if (result > s)
+    {
+        end = mid - 1;
+    }
+    else
+    {
+        start = mid + 1;
+    }
+}
+
+Console.WriteLine(end);
---
+++ BackJoon/2217.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+int w = 0;
+List<int> ropes = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    w = int.Parse(Console.ReadLine());
+    ropes.Add(w);
+}
+
+ropes.Sort();
+int max = int.MinValue;
+int value = 0;
+
+for (int i = 0; i < n; i++)
+{
+    value = ropes[i] * (n - i);
+    if (max < value)
+    {
+        max = value;
+    }
+}
+
+Console.WriteLine(max);
