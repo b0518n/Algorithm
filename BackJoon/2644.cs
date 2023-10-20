a502068f067e649578b618b4fdbd441cf863502b
b0518n <b0518n@naver.com>
2023-10-20 18:36:09 +0900

    BFS

---
+++ BackJoon/2644.cs
@@ -0,0 +1,51 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int a = input[0];
+int b = input[1];
+int m = int.Parse(Console.ReadLine());
+int x = 0;
+int y = 0;
+int result = -1;
+List<List<int>> list = new List<List<int>>();
+int[] visited = new int[n + 1];
+
+for (int i = 0; i < n + 1; i++)
+{
+    list.Add(new List<int>());
+}
+
+for (int i = 0; i < m; i++)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    x = input[0]; // 부모
+    y = input[1]; // 자식
+
+    list[x].Add(y);
+    list[y].Add(x);
+}
+
+visited[a] = 1;
+BFS(a, 0);
+
+Console.WriteLine(result);
+
+void BFS(int index, int value)
+{
+    if (index == b)
+    {
+        result = value;
+        return;
+    }
+
+    foreach (int i in list[index])
+    {
+        if (visited[i] == 1)
+        {
+            continue;
+        }
+
+        visited[i] = 1;
+        BFS(i, value + 1);
+        visited[i] = 0;
+    }
+}
