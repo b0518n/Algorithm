d6e9ef10257aeaea36d0987e9d0d43a7ce2c9c1e
b0518n <b0518n@naver.com>
2023-11-08 20:50:58 +0900

    Greedy Algorithm

---
+++ BackJoon/1449.cs
@@ -0,0 +1,34 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0]; // 물이 새는 곳의 개수
+int l = input[1]; // 테이프의 길이
+
+input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+List<int> holes = new List<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    holes.Add(input[i]);
+}
+
+holes.Sort();
+Dictionary<int, int> dics = new Dictionary<int, int>();
+int count = 0;
+
+for (int i = 0; i < holes.Count; i++)
+{
+    if (!dics.ContainsKey(holes[i]))
+    {
+        count++;
+        dics.Add(holes[i], 1);
+        for (int j = 1; j < l; j++)
+        {
+            dics.Add(holes[i] + j, 1);
+        }
+    }
+    else
+    {
+        continue;
+    }
+}
+
+Console.WriteLine(count);
