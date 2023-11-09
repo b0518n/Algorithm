ff4314f5e7a3581a73542efd3c1f3eec3f14a09c
b0518n <b0518n@naver.com>
2023-11-09 13:56:01 +0900

    DFS

---
+++ BackJoon/9466.cs
@@ -0,0 +1,58 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n = 0;
+int[] input = null;
+int[] dp = null;
+int[] visited = null;
+int count = 0;
+
+for (int i = 0; i < t; i++)
+{
+    n = int.Parse(Console.ReadLine());
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    dp = new int[n + 1];
+    visited = new int[n + 1];
+    count = 0;
+
+    for (int j = 0; j < n; j++)
+    {
+        dp[j + 1] = input[j];
+    }
+
+    for (int k = 1; k < n + 1; k++)
+    {
+        if (visited[k] == 0)
+        {
+            DFS(k);
+        }
+    }
+
+    for (int k = 1; k < n + 1; k++)
+    {
+        if (visited[k] != -1)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine(count);
+}
+
+void DFS(int index)
+{
+    int _index = index;
+
+    while (true)
+    {
+        if (visited[_index] != 0)
+        {
+            while (visited[_index] != -1 && visited[_index] == index)
+            {
+                visited[_index] = -1;
+                _index = dp[_index];
+            }
+            return;
+        }
+        visited[_index] = index;
+        _index = dp[_index];
+    }
+}
