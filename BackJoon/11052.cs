1800c84ee822c89ccae4271c24ed9910b1f3454d
b0518n <b0518n@naver.com>
2023-11-17 06:03:09 +0900

    Dynamic Programming

---
+++ BackJoon/11052.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] dp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+List<int> temp = dp.ToList();
+temp.Insert(0, 0);
+dp = temp.ToArray();
+
+for (int i = 2; i < n + 1; i++)
+{
+    for (int j = 1; j < i + 1; j++)
+    {
+        dp[i] = Math.Max(dp[j] + dp[i - j], dp[i]);
+    }
+}
+
+Console.WriteLine(dp[n]);
---
+++ BackJoon/2748.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+long[] dp = new long[91];
+
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 1;
+
+for (int i = 3; i < n + 1; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+Console.WriteLine(dp[n]);
