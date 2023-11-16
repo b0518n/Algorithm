6c98ff571e3c0213b936329197dd217ea64a0055
b0518n <b0518n@naver.com>
2023-11-16 17:47:57 +0900

    Dynamic Programming

---
+++ BackJoon/2193.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+long[] dp = new long[91];
+dp[1] = 1;
+dp[2] = 1;
+
+for (int i = 3; i <= n; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+Console.WriteLine(dp[n]);
