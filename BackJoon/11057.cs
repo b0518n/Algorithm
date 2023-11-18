8f6deaa6bde7e01134e0d5ee2d7e96120512895e
b0518n <b0518n@naver.com>
2023-11-18 14:34:44 +0900

    Dynamic Programing

---
+++ BackJoon/11057.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] dp = new int[1001];
+int[] arr = new int[11];
+dp[1] = 10;
+
+for (int i = 1; i < 11; i++)
+{
+    arr[i] = i;
+}
+
+for (int i = 2; i < 1001; i++)
+{
+    for (int j = 2; j < 11; j++)
+    {
+        arr[j] = (arr[j - 1] + arr[j]) % 10007;
+    }
+
+    dp[i] = arr[10];
+}
+
+Console.WriteLine(dp[n]);
---
+++ BackJoon/9095.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int t = int.Parse(Console.ReadLine());
+int n = 0;
+int[] dp = new int[11];
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+
+for (int i = 4; i < 11; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+}
+
+for (int i = 0; i < t; i++)
+{
+    n = int.Parse(Console.ReadLine());
+    sb.AppendLine(dp[n].ToString());
+}
+
+Console.WriteLine(sb.ToString());
