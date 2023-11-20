223cac40a9fb93f538485096934e3b9c881ed628
b0518n <b0518n@naver.com>
2023-11-21 02:44:55 +0900

    Dynamic Programming

---
+++ BackJoon/11722.cs
@@ -0,0 +1,41 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+List<int> list = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    if (list.Count == 0)
+    {
+        list.Add(input[i]);
+    }
+    else
+    {
+        for (int j = list.Count - 1; j >= 0; j--)
+        {
+            if (list[j] < input[i])
+            {
+                if (j == 0)
+                {
+                    list[j] = input[i];
+                }
+
+                continue;
+            }
+            else if (list[j] > input[i])
+            {
+                if (j == list.Count - 1)
+                {
+                    list.Add(input[i]);
+                }
+                else
+                {
+                    list[j + 1] = input[i];
+                }
+
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(list.Count);
---
+++ BackJoon/1699.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] dp = new int[100001];
+for (int i = 1; i <= 316; i++)
+{
+    dp[i * i] = 1;
+}
+
+int count = 1;
+
+for (int i = 2; i < 100001; i++)
+{
+    if (dp[i] != 0)
+    {
+        count++;
+        continue;
+    }
+
+    for (int j = count; j >= 0; j--)
+    {
+        if (dp[i] == 0)
+        {
+            dp[i] = dp[j * j] + dp[i - (j * j)];
+        }
+        else
+        {
+            dp[i] = Math.Min(dp[i], dp[j * j] + dp[i - (j * j)]);
+        }
+    }
+}
+
+Console.WriteLine(dp[n]);
