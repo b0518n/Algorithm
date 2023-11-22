a4a192c24a052d4fcd7231ac7395acc596ee7acb
b0518n <b0518n@naver.com>
2023-11-22 22:36:53 +0900

    Dynamic Programming

---
+++ BackJoon/9655.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int nmg = n % 2;
+string result = string.Empty;
+
+if (nmg == 0)
+{
+    result = "CY";
+}
+else
+{
+    result = "SK";
+}
+
+Console.WriteLine(result);
