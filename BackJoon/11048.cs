709796e15be14ed348a7bef721f65a9f0c4e5f61
b0518n <b0518n@naver.com>
2023-11-24 17:32:24 +0900

    Dynamic Programming

---
+++ BackJoon/11048.cs
@@ -0,0 +1,35 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+int[,] dp = new int[n, m];
+
+int[] temp = null;
+int max = 0;
+
+for (int i = 0; i < n; i++)
+{
+    temp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < m; j++)
+    {
+        dp[i, j] = temp[j];
+
+        max = 0;
+
+        if (i - 1 >= 0 && j - 1 >= 0)
+        {
+            max = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+        }
+        else if (i - 1 >= 0 && j - 1 < 0)
+        {
+            max = dp[i - 1, j];
+        }
+        else if (i - 1 < 0 && j - 1 >= 0)
+        {
+            max = dp[i, j - 1];
+        }
+
+        dp[i, j] += max;
+    }
+}
+
+Console.WriteLine(dp[n - 1, m - 1]);
---
+++ BackJoon/2225.cs
@@ -0,0 +1,23 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int k = input[1];
+
+int[,] dp = new int[201, 201];
+
+for (int i = 0; i < 201; i++)
+{
+    dp[1, i] = 1;
+    dp[i, 0] = 1;
+}
+
+dp[0, 0] = 0;
+
+for (int i = 1; i < 201; i++)
+{
+    for (int j = 1; j < 201; j++)
+    {
+        dp[i, j] = (dp[i, j - 1] + dp[i - 1, j]) % 1000000000;
+    }
+}
+
+Console.WriteLine(dp[k, n]);
