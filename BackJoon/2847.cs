9243b990d340cc95dc62e7e9b4238e26262c66b6
b0518n <b0518n@naver.com>
2023-12-05 20:08:26 +0900

    Greedy Algorithm

---
+++ BackJoon/2847.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] scores = new int[n];
+int input = 0;
+
+for (int i = 0; i < n; i++)
+{
+    input = int.Parse(Console.ReadLine());
+    scores[i] = input;
+}
+
+int value = scores[n - 1];
+int result = 0;
+
+for (int i = n - 2; i >= 0; i--)
+{
+    while (true)
+    {
+        if (scores[i] >= value)
+        {
+            result++;
+            scores[i]--;
+        }
+        else
+        {
+            value = scores[i];
+            break;
+        }
+    }
+}
+
+Console.WriteLine(result);
