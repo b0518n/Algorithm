cf50bd3d731c9113a5baa7a7c0997546465a6bd2
b0518n <b0518n@naver.com>
2023-12-09 23:09:48 +0900

    Greedy Algorithm

---
+++ BackJoon/12904.cs
@@ -0,0 +1,24 @@
+﻿string s = Console.ReadLine();
+string t = Console.ReadLine();
+
+while (t.Length > s.Length)
+{
+    if (t[t.Length - 1] == 'A')
+    {
+        t = t.Substring(0, t.Length - 1);
+    }
+    else
+    {
+        t = t.Substring(0, t.Length - 1);
+        t = new string(t.Reverse().ToArray());
+    }
+}
+
+if (s == t)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    Console.WriteLine(0);
+}
