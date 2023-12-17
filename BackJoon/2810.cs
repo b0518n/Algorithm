062a38895543345081852c7182e3eea8673cacc9
b0518n <b0518n@naver.com>
2023-12-17 22:23:13 +0900

    Greedy Algorithm

---
+++ BackJoon/2810.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+string str = Console.ReadLine();
+int result = 1;
+
+for (int i = 0; i < n; i++)
+{
+    if (str[i] == 'S')
+    {
+        result++;
+    }
+    else
+    {
+        i++;
+        result++;
+    }
+}
+
+result = Math.Min(result, n);
+Console.WriteLine(result);
