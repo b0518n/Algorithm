2a64cccb2c016ce13117d5408632e023218ed86a
b0518n <b0518n@naver.com>
2023-12-24 23:47:00 +0900

    Greedy Algorithm

---
+++ BackJoon/1417.cs
@@ -0,0 +1,63 @@
+﻿int n = int.Parse(Console.ReadLine());
+int value = int.Parse(Console.ReadLine());
+int max = -1;
+Dictionary<int, int> dics = new Dictionary<int, int>();
+int input = 0;
+int result = 0;
+
+for (int i = 1; i < n; i++)
+{
+    input = int.Parse(Console.ReadLine());
+
+    if (dics.ContainsKey(input))
+    {
+        dics[input]++;
+    }
+    else
+    {
+        dics.Add(input, 1);
+        max = Math.Max(max, input);
+    }
+}
+
+while (true)
+{
+    if (max >= value)
+    {
+        value++;
+        if (dics[max] > 1)
+        {
+            dics[max]--;
+            if (dics.ContainsKey(max - 1))
+            {
+                dics[max - 1]++;
+            }
+            else
+            {
+                dics.Add(max - 1, 1);
+            }
+        }
+        else
+        {
+            dics.Remove(max);
+            if (dics.ContainsKey(max - 1))
+            {
+                dics[max - 1]++;
+            }
+            else
+            {
+                dics.Add(max - 1, 1);
+            }
+
+            max--;
+        }
+
+        result++;
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine(result);
