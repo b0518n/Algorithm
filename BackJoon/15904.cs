8cb902ee07baa2e8d899ac9af60878ef7c9ed5d2
b0518n <b0518n@naver.com>
2023-12-29 06:38:42 +0900

    Greedy Algorithm

---
+++ BackJoon/15904.cs
@@ -0,0 +1,29 @@
+﻿string input = Console.ReadLine();
+int length = input.Length;
+int index = 0;
+char[] arr = { 'U', 'C', 'P', 'C' };
+
+for (int i = 0; i < length; i++)
+{
+    if (index == 4)
+    {
+        Console.WriteLine("I love UCPC");
+        break;
+    }
+
+    if (arr[index] == input[i])
+    {
+        index++;
+    }
+
+    if (i == length - 1)
+    {
+        if (index == 4)
+        {
+            Console.WriteLine("I love UCPC");
+            break;
+        }
+
+        Console.WriteLine("I hate UCPC");
+    }
+}
