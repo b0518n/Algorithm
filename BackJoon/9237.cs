7b8692534e52db1aae79ad611bbb55f7272e1b8b
b0518n <b0518n@naver.com>
2024-01-03 13:55:10 +0900

    Greedy Algorithm

---
+++ BackJoon/9237.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Array.Sort(input);
+Array.Reverse(input);
+
+int max = -1;
+int result = 1;
+
+for (int i = 0; i < n; i++)
+{
+    if (i == 0)
+    {
+        max = input[i];
+        result++;
+    }
+    else
+    {
+        max--;
+        max = Math.Max(max, input[i]);
+        result++;
+    }
+}
+
+result += max;
+Console.WriteLine(result);
