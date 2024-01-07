a5cb70533c32a2b74b645b7f984749898f4091eb
b0518n <b0518n@naver.com>
2024-01-07 18:39:37 +0900

    Greedy Algorithm

---
+++ BackJoon/14720.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int value = 0;
+int result = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (input[i] == value)
+    {
+        if (value == 2)
+        {
+            value = 0;
+        }
+        else
+        {
+            value++;
+        }
+
+        result++;
+    }
+}
+
+Console.WriteLine(result);
