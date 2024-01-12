87e94e156580de576c7a9b86e74dcccacdcc4da8
b0518n <b0518n@naver.com>
2024-01-12 22:46:19 +0900

    Greedy Algorithm

---
+++ BackJoon/19939.cs
@@ -0,0 +1,26 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int k = input[1];
+int result = 0;
+
+int min = k * (k + 1) / 2;
+
+
+if (n < min)
+{
+    result = -1;
+}
+else
+{
+    int value = n - min;
+    if (value % k == 0)
+    {
+        result = k - 1;
+    }
+    else
+    {
+        result = k;
+    }
+}
+
+Console.WriteLine(result);
