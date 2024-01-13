73665eaded96630fef32556ef5e59dcd46a701e7
b0518n <b0518n@naver.com>
2024-01-13 23:40:09 +0900

    Brute Force

---
+++ BackJoon/10971.cs
@@ -0,0 +1,84 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] input = null;
+int[,] costs = new int[n, n];
+int minCost = -1;
+
+for (int i = 0; i < n; i++)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < n; j++)
+    {
+        costs[i, j] = input[j];
+    }
+}
+
+int[] check = new int[n];
+List<int> sequence = new List<int>();
+Permutation();
+Console.WriteLine(minCost);
+
+void Permutation()
+{
+    if (sequence.Count == n - 1)
+    {
+        CalcuateMinCost();
+        return;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (check[i] == 0)
+        {
+            sequence.Add(i);
+            check[i] = 1;
+            Permutation();
+            check[i] = 0;
+            sequence.RemoveAt(sequence.Count - 1);
+        }
+    }
+}
+
+void CalcuateMinCost()
+{
+    int value = 0;
+
+    for (int i = 0; i < sequence.Count; i++)
+    {
+        if (i == 0)
+        {
+            if (costs[0, sequence[i]] == 0)
+            {
+                return;
+            }
+
+            value += costs[0, sequence[i]];
+        }
+        else
+        {
+            if (costs[sequence[i - 1], sequence[i]] == 0)
+            {
+                return;
+            }
+
+            value += costs[sequence[i - 1], sequence[i]];
+        }
+    }
+
+    if (costs[sequence[sequence.Count - 1], 0] == 0)
+    {
+        return;
+    }
+
+    value += costs[sequence[sequence.Count - 1], 0];
+
+    if (minCost == -1)
+    {
+        minCost = value;
+    }
+    else
+    {
+        minCost = Math.Min(minCost, value);
+    }
+
+    return;
+}
