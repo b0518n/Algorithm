31826b5dde5b1c8e5f6a058c39bb7fae8ca73159
b0518n <b0518n@naver.com>
2024-01-28 22:59:01 +0900

    DFS

--- BackJoon/1987.cs
+++ BackJoon/1987.cs
@@ -2,53 +2,52 @@
 int r = input[0];
 int c = input[1];
 
+string[,] board = new string[r, c];
+int[,] visited = new int[r, c];
+Dictionary<string, int> dics = new Dictionary<string, int>();
+
 string str = null;
-string[,] dp = new string[r, c];
+int result = -1;
+
 for (int i = 0; i < r; i++)
 {
     str = Console.ReadLine();
     for (int j = 0; j < c; j++)
     {
-        dp[i, j] = str[j].ToString();
+        board[i, j] = str[j].ToString();
     }
 }
-
-Dictionary<string, int> dics = new Dictionary<string, int>();
 
 int[] dy = new int[4] { -1, 1, 0, 0 };
 int[] dx = new int[4] { 0, 0, -1, 1 };
 
-int max = -1;
-
-dics.Add(dp[0, 0], 1);
+visited[0, 0] = 1;
+dics.Add(board[0, 0], 1);
 DFS(0, 0, 1);
 
-Console.WriteLine(max);
+Console.WriteLine(result);
 
-void DFS(int y, int x, int result)
+void DFS(int y, int x, int cnt)
 {
-    max = Math.Max(max, result);
-
     int ny = 0;
     int nx = 0;
+
+    result = Math.Max(result, cnt);
 
     for (int i = 0; i < 4; i++)
     {
         ny = y + dy[i];
         nx = x + dx[i];
 
-        if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+        if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny, nx] == 1 || dics.ContainsKey(board[ny, nx]))
         {
             continue;
         }
 
-        if (dics.ContainsKey(dp[ny, nx]))
-        {
-            continue;
-        }
-
-        dics.Add(dp[ny, nx], 1);
-        DFS(ny, nx, result + 1);
-        dics.Remove(dp[ny, nx]);
+        visited[ny, nx] = 1;
+        dics.Add(board[ny, nx], 1);
+        DFS(ny, nx, cnt + 1);
+        dics.Remove(board[ny, nx]);
+        visited[ny, nx] = 0;
     }
 }
