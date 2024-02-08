499f4cd706a65efe3c9925e4ab762e39f18dbb34
b0518n <b0518n@naver.com>
2024-02-08 23:24:21 +0900

    Dynamic Programming

---
+++ BackJoon/1003.cs
@@ -0,0 +1,34 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+Info[] dp = new Info[41];
+dp[0] = new Info(1, 0);
+dp[1] = new Info(0, 1);
+
+for (int i = 2; i < 41; i++)
+{
+    dp[i] = new Info(dp[i - 1].zeroCnt + dp[i - 2].zeroCnt, dp[i - 1].oneCnt + dp[i - 2].oneCnt);
+}
+
+int t = int.Parse(sr.ReadLine());
+int n = 0;
+
+for (int i = 0; i < t; i++)
+{
+    n = int.Parse(sr.ReadLine());
+    sw.WriteLine($"{dp[n].zeroCnt} {dp[n].oneCnt}");
+}
+
+sw.Flush();
+sw.Close();
+
+class Info
+{
+    public int zeroCnt;
+    public int oneCnt;
+
+    public Info(int zeroCnt, int oneCnt)
+    {
+        this.zeroCnt = zeroCnt;
+        this.oneCnt = oneCnt;
+    }
+}
