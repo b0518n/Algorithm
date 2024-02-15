27d01626f301a9c4038c77d63093fb1f0eb44cb5
b0518n <b0518n@naver.com>
2024-02-15 23:41:32 +0900

    Sort

---
+++ BackJoon/11004.cs
@@ -0,0 +1,11 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int k = input[1];
+
+input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+Array.Sort(input);
+sw.WriteLine(input[k - 1]);
+sw.Flush();
+sw.Close();
