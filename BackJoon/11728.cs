ad6447e60b718170f243455b3bf23aad910bf592
b0518n <b0518n@naver.com>
2024-02-16 23:25:46 +0900

    Sort

---
+++ BackJoon/11728.cs
@@ -0,0 +1,44 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+
+int[] arrA = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int[] arrB = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+Array.Sort(arrA);
+Array.Sort(arrB);
+
+List<int> container = new List<int>();
+
+int indexA = 0;
+int indexB = 0;
+
+while (indexA <= n - 1 || indexB <= m - 1)
+{
+    if (indexA <= n - 1 && indexB <= m - 1 && arrA[indexA] > arrB[indexB])
+    {
+        container.Add(arrB[indexB]);
+        indexB++;
+    }
+    else if (indexA <= n - 1 && indexB <= m - 1 && arrA[indexA] <= arrB[indexB])
+    {
+        container.Add(arrA[indexA]);
+        indexA++;
+    }
+    else if (indexA > n - 1)
+    {
+        container.Add(arrB[indexB]);
+        indexB++;
+    }
+    else if (indexB > m - 1)
+    {
+        container.Add(arrA[indexA]);
+        indexA++;
+    }
+}
+
+sw.WriteLine(string.Join(" ", container));
+sw.Flush();
+sw.Close();
