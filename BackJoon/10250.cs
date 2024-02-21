5daf9471e63fd9ddc318099b0f4ee9216040595f
b0518n <b0518n@naver.com>
2024-02-21 22:48:18 +0900

    Math

---
+++ BackJoon/10250.cs
@@ -0,0 +1,47 @@
+﻿int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string[] input = Console.ReadLine().Split(" ");
+    int h = int.Parse(input[0]);
+    int w = int.Parse(input[1]);
+    int n = int.Parse(input[2]);
+
+    int mok = n / h;
+    int nmg = n % h;
+
+    if (mok < 9)
+    {
+        if (nmg == 0)
+        {
+            Console.WriteLine(h.ToString() + "0" + mok.ToString());
+        }
+        else
+        {
+            Console.WriteLine(nmg.ToString() + "0" + (mok + 1).ToString());
+        }
+    }
+    else if (mok == 9)
+    {
+        if (nmg == 0)
+        {
+            Console.WriteLine(h.ToString() + "0" + mok.ToString());
+        }
+        else
+        {
+            Console.WriteLine(nmg.ToString() + (mok + 1).ToString());
+        }
+    }
+    else if (mok > 9)
+    {
+        if (nmg == 0)
+        {
+            Console.WriteLine(h.ToString() + mok.ToString());
+        }
+        else
+        {
+            Console.WriteLine(nmg.ToString() + (mok + 1).ToString());
+        }
+    }
+
+}
+
