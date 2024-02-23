fa10e1a530edc4532c0237f5227a237e77e6ef51
b0518n <b0518n@naver.com>
2024-02-23 19:36:23 +0900

    BitMasking

---
+++ BackJoon/1094.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 0; i < 7; i++)
+{
+    if ((n & (1 << i)) == (1 << i))
+    {
+        result++;
+    }
+}
+
+Console.WriteLine(result);
