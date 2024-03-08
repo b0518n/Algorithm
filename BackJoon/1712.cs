001760c4edd9e5367ca24a7e0c9b5861822d377d
kimdongin <b0518n@naver.com>
2024-03-08 23:19:53 +0900

    Math

---
+++ BackJoon/1712.cs
@@ -0,0 +1,16 @@
+﻿string[] str = Console.ReadLine().Split(" ");
+int a = int.Parse(str[0]);
+int b = int.Parse(str[1]);
+int c = int.Parse(str[2]);
+
+int retValue = 0;
+
+if (c - b <= 0)
+{
+    Console.WriteLine("-1");
+}
+else
+{
+    retValue = (a / (c - b)) + 1;
+    Console.WriteLine(retValue);
+}
