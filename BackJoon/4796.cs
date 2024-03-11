f230a191fa0a946a4634e2cbd29208d5dd066f39
kimdongin <b0518n@naver.com>
2024-03-11 23:51:34 +0900

    Greedy Algorithm

---
+++ BackJoon/4796.cs
@@ -0,0 +1,39 @@
+﻿int[] input = null;
+int p = 0;
+int l = 0;
+int v = 0;
+
+int result = 0;
+int mok = 0;
+int nmg = 0;
+int i = 1;
+
+while (true)
+{
+    input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    l = input[0];
+    p = input[1];
+    v = input[2];
+
+    if (l == 0 && p == 0 && v == 0)
+    {
+        break;
+    }
+
+    mok = v / p;
+    nmg = v % p;
+
+    result += l * mok;
+    if (nmg >= l)
+    {
+        result += l;
+    }
+    else
+    {
+        result += nmg;
+    }
+
+    Console.WriteLine($"Case {i}: {result}");
+    i++;
+    result = 0;
+}
