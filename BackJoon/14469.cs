a4f51a902a3591225ae85ffbd211b5c52ce88d5e
kimdongin <b0518n@naver.com>
2024-03-20 08:46:30 +0900

    Greedy Algorithm

---
+++ BackJoon/14469.cs
@@ -0,0 +1,55 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+List<CowInfo> list = new List<CowInfo>();
+
+int n = int.Parse(sr.ReadLine());
+int[] input = null;
+
+for (int i = 0; i < n; i++)
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    list.Add(new CowInfo(input[0], input[1]));
+}
+
+list.Sort((x, y) =>
+{
+    int compare = x.arrivedTime.CompareTo(y.arrivedTime);
+
+    if (compare == 0)
+    {
+        compare = x.consumedTime.CompareTo(y.consumedTime);
+    }
+
+    return compare;
+});
+
+int currentTime = 0;
+int index = 0;
+
+while (index <= list.Count - 1)
+{
+    if (currentTime <= list[index].arrivedTime)
+    {
+        currentTime = list[index].arrivedTime;
+    }
+
+    currentTime += list[index].consumedTime;
+    index++;
+}
+
+sw.WriteLine(currentTime);
+sw.Flush();
+sw.Close();
+
+class CowInfo
+{
+    public int arrivedTime;
+    public int consumedTime;
+
+    public CowInfo(int arrivedTime, int consumedTime)
+    {
+        this.arrivedTime = arrivedTime;
+        this.consumedTime = consumedTime;
+    }
+}
