89d7364d8d0a947b89f81eb86de78d819e42733c
kimdongin <b0518n@naver.com>
2024-03-23 07:05:35 +0900

    Greedy Algorithm

---
+++ BackJoon/14487.cs
@@ -0,0 +1,32 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = 0;
+int[] input = null;
+
+int max = -1;
+int sum = 0;
+
+Input();
+GetMaxCostAndSum();
+Print();
+
+void Input()
+{
+    n = int.Parse(sr.ReadLine());
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+}
+void GetMaxCostAndSum()
+{
+    for (int i = 0; i < n; i++)
+    {
+        max = Math.Max(max, input[i]);
+        sum += input[i];
+    }
+}
+void Print()
+{
+    sw.WriteLine(sum - max);
+    sw.Flush();
+    sw.Close();
+}
