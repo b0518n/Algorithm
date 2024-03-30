04346f5a15c78096909f82a911143ca3c649cb98
kimdongin <b0518n@naver.com>
2024-03-30 23:38:31 +0900

    Math

---
+++ BackJoon/2775.cs
@@ -0,0 +1,29 @@
+﻿int t = int.Parse(Console.ReadLine());
+int[] arr = new int[210];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i >= 0 && i <= 13)
+    {
+        arr[i] = i + 1;
+    }
+    else
+    {
+        if (i % 14 == 0)
+        {
+            arr[i] = 1;
+        }
+        else
+        {
+            arr[i] = arr[i - 1] + arr[i - 14];
+        }
+    }
+}
+
+for (int i = 0; i < t; i++)
+{
+    int k = int.Parse(Console.ReadLine());
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(arr[(k * 14) + (n - 1)]);
+}
