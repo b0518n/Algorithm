abc752226eeb33d3fd0191d6b3919bc5ef48a472
kimdongin <b0518n@naver.com>
2024-04-01 20:25:04 +0900

    Greedy Algorithm

---
+++ BackJoon/7570.cs
@@ -0,0 +1,47 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int[] dp = new int[n + 1];
+Dictionary<int, int> dic = new Dictionary<int, int>();
+int result = 0;
+
+GetMinMove();
+Print();
+
+void GetMinMove()
+{
+    int retValue = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        dic.Add(arr[i], i);
+    }
+
+    for (int i = 2; i < n + 1; i++)
+    {
+        if (dic[i] > dic[i - 1])
+        {
+            dp[i] = dp[i - 1] + 1;
+        }
+
+        if (retValue == 0)
+        {
+            retValue = dp[i];
+        }
+        else
+        {
+            retValue = Math.Max(retValue, dp[i]);
+        }
+    }
+
+    retValue++;
+    result = n - retValue;
+}
+void Print()
+{
+    sw.WriteLine(result);
+    sw.Flush();
+    sw.Close();
+}
