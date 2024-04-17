6758d7672ea5f01ecd7374e82788e7d97c08c34a
kimdongin <b0518n@naver.com>
2024-04-17 22:34:28 +0900

    string

---
+++ BackJoon/8958.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    int value = 0;
+    int previousValue = 0;
+    string str = Console.ReadLine();
+    for (int j = 0; j < str.Length; j++)
+    {
+        if (j == 0)
+        {
+            if (str[j].ToString().Equals("O"))
+            {
+                value += 1;
+                previousValue = 1;
+            }
+        }
+        else
+        {
+            if (str[j].ToString().Equals("O"))
+            {
+                value += previousValue + 1;
+                previousValue = previousValue + 1;
+            }
+            else
+            {
+                previousValue = 0;
+            }
+        }
+
+    }
+    Console.WriteLine(value);
+}
