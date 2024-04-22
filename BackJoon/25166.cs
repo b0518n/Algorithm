afb85c1893aa7f510a6cb056b8e6cb1269cbdde8
kimdongin <b0518n@naver.com>
2024-04-22 13:31:51 +0900

    BitMasking

---
+++ BackJoon/25166.cs
@@ -0,0 +1,53 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int[] input = null;
+int s = 0;
+int m = 0;
+string result = string.Empty;
+
+Input();
+PayforSandwich();
+Print();
+
+void Input()
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    s = input[0];
+    m = input[1];
+}
+void PayforSandwich()
+{
+    int tValue;
+
+    if (s <= 1023)
+    {
+        result = "No thanks";
+    }
+    else
+    {
+        tValue = s - 1023;
+        if ((tValue & m) == 0)
+        {
+            result = "Impossible";
+        }
+        else
+        {
+            tValue = tValue - (tValue & m);
+            if (tValue == 0)
+            {
+                result = "Thanks";
+            }
+            else
+            {
+                result = "Impossible";
+            }
+        }
+    }
+}
+void Print()
+{
+    sw.WriteLine(result);
+    sw.Flush();
+    sw.Close();
+}
---
+++ BackJoon/28239.cs
@@ -0,0 +1,39 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+List<long> list = new List<long>();
+List<int> result = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    list.Add(long.Parse(sr.ReadLine()));
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < 64; j++)
+    {
+        if ((list[i] & (long)1 << j) != 0)
+        {
+            result.Add(j);
+        }
+    }
+
+    if (result.Count == 1)
+    {
+        int tValue = result[0] - 1;
+        result.RemoveAt(0);
+        result.Add(tValue);
+        result.Add(tValue);
+        sw.WriteLine(string.Join(" ", result));
+    }
+    else
+    {
+        sw.WriteLine(string.Join(" ", result));
+    }
+    result.Clear();
+}
+
+sw.Flush();
+sw.Close();
