fecae9732f556a0c82e58f4e70a2309d33f240ba
kimdongin <b0518n@naver.com>
2024-04-30 23:56:40 +0900

    Binary Search

---
+++ BackJoon/13777.cs
@@ -0,0 +1,50 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int input = 0;
+
+
+while (true)
+{
+    input = int.Parse(sr.ReadLine());
+    if (input == 0)
+    {
+        break;
+    }
+    else
+    {
+        SearchValue();
+    }
+}
+
+sw.Flush();
+sw.Close();
+
+void SearchValue()
+{
+    int start = 1;
+    int end = 50;
+    int mid = 0;
+
+
+    while (true)
+    {
+        mid = (start + end) / 2;
+
+        if (mid == input)
+        {
+            sw.WriteLine(mid);
+            break;
+        }
+
+        sw.Write(mid + " ");
+        if (mid > input)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+}
