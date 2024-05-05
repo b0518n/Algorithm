50e21c74bf4540a9c7cbd573cae764282688bd3d
kimdongin <b0518n@naver.com>
2024-05-05 17:09:36 +0900

    Binary Search

---
+++ BackJoon/2417.cs
@@ -0,0 +1,27 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+long n = long.Parse(Console.ReadLine());
+sw.WriteLine(BinarySearch());
+sw.Flush();
+sw.Close();
+
+long BinarySearch()
+{
+    long left = 0;
+    long right = n;
+    long middle = 0;
+
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (Math.Pow(middle, 2) >= n)
+        {
+            right = middle - 1;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+
+    return left;
+}
