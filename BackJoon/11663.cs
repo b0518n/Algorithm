e609b94b96ca8857fad8fb6f9dbf058cc4e4b03c
kimdongin <b0518n@naver.com>
2024-05-15 14:05:16 +0900

    Binary Search

---
+++ BackJoon/11663.cs
@@ -0,0 +1,69 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+
+int result = 0;
+
+int[] dotArr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+Array.Sort(dotArr);
+for (int i = 0; i < m; i++)
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    result = GetDotCnt(input[0], input[1]);
+    sw.WriteLine(result);
+}
+
+sw.Flush();
+sw.Close();
+
+int GetDotCnt(int min, int max)
+{
+    int minIndex = GetMinIndex(min);
+    int maxIndex = GetMaxIndex(max);
+
+    return maxIndex - minIndex;
+}
+int GetMinIndex(int value)
+{
+    int left = 0;
+    int right = n - 1;
+    int middle = 0;
+
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (value > dotArr[middle])
+        {
+            left = middle + 1;
+        }
+        else
+        {
+            right = middle - 1;
+        }
+    }
+
+    return left;
+}
+int GetMaxIndex(int value)
+{
+    int left = 0;
+    int right = n - 1;
+    int middle = 0;
+
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (value >= dotArr[middle])
+        {
+            left = middle + 1;
+        }
+        else
+        {
+            right = middle - 1;
+        }
+    }
+
+    return left;
+}
