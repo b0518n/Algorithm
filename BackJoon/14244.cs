d3bdddb5263b66a1923002c0e4f2392fedfcb0bb
kimdongin <b0518n@naver.com>
2024-05-20 14:57:12 +0900

    Tree

---
+++ BackJoon/14244.cs
@@ -0,0 +1,24 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int m = input[1];
+
+int leafCnt = 0;
+
+for (int i = 1; i < n; i++)
+{
+    if (m > leafCnt)
+    {
+        sw.WriteLine($"0 {i}");
+        leafCnt++;
+    }
+    else
+    {
+        sw.WriteLine($"{i - 1} {i}");
+    }
+}
+
+sw.Flush();
+sw.Close();
