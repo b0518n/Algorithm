a547b49c2db2f1d65553de054d9e50d54a96edf0
kimdongin <b0518n@naver.com>
2024-07-12 18:49:31 +0900

    Greedy Algorithm

---
+++ BackJoon/13417.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int t = int.Parse(sr.ReadLine());
+int n = 0;
+char[] input = null;
+StringBuilder sb = new StringBuilder();
+char firstChar = 'a';
+
+for (int i = 0; i < t; i++)
+{
+    n = int.Parse(sr.ReadLine());
+    input = Array.ConvertAll(sr.ReadLine().Split(), char.Parse);
+    for (int j = 0; j < n; j++)
+    {
+        if (j == 0)
+        {
+            sb.Append(input[j]);
+            firstChar = input[j];
+        }
+        else
+        {
+            if (firstChar >= input[j])
+            {
+                sb.Insert(0, input[j]);
+                firstChar = input[j];
+            }
+            else
+            {
+                sb.Append(input[j]);
+            }
+        }
+    }
+
+    sw.WriteLine(sb.ToString());
+    sb.Clear();
+}
+
+sw.Flush();
+sw.Close();
