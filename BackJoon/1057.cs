61dc800dd2854c3d7c39815f578f6ba77b4f8966
kimdongin <b0518n@naver.com>
2024-07-17 21:31:21 +0900

    Brute Force

---
+++ BackJoon/1057.cs
@@ -0,0 +1,19 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = input[0];
+int jimin = input[1];
+int hansu = input[2];
+int result = 0;
+
+while (jimin != hansu)
+{
+    result++;
+    jimin = (jimin + 1) / 2;
+    hansu = (hansu + 1) / 2;
+}
+
+sw.Write(result);
+sw.Flush();
+sw.Close();
