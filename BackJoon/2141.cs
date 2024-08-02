63d3df96f657a26c870145f70c3c6e628f401327
kimdongin <b0518n@naver.com>
2024-08-02 14:03:11 +0900

    Greedy Algorithm

---
+++ BackJoon/2141.cs
@@ -0,0 +1,42 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+int[] input = null;
+List<Info> list = new List<Info>();
+long total = 0;
+
+for (int i = 0; i < n; i++)
+{
+    input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    list.Add(new Info(input[0], input[1]));
+    total += (long)input[1];
+}
+
+list.Sort((x, y) => x.dest.CompareTo(y.dest));
+long cnt = 0;
+
+for (int i = 0; i < n; i++)
+{
+    cnt += list[i].cnt;
+    if (cnt >= (total + 1) / 2)
+    {
+        sw.WriteLine(list[i].dest);
+        break;
+    }
+}
+
+sw.Flush();
+sw.Close();
+
+class Info
+{
+    public long dest;
+    public long cnt;
+
+    public Info(long _dest, long _cnt)
+    {
+        this.dest = _dest;
+        this.cnt = _cnt;
+    }
+}
